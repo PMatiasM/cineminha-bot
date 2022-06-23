a953df313954b28bba43026daf17ffa3bfa4feeb
PMatiasM <pedromm2406@gmail.com>
2022-06-23 11:12:38 -0300

    fix: Bot Activity

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,9 @@ module.exports = {
   once: true,
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
-    client.user.setActivity("/help", { type: "WATCHING" });
+    setInterval(
+      () => client.user.setActivity("/help", { type: "WATCHING" }),
+      7200000
+    );
   },
 };
